ed336c45ee75486fc2dd001d64f18df46bc1e73c
Kyle Throssell <kthrossell@tetrascience.com>
2025-12-11 09:01:28 -0500

    bumping the version

--- chrono_simplex/__init__.py
+++ chrono_simplex/__init__.py
@@ -6,5 +6,5 @@ from .formatter import format_time
 from .logging_setup import setup_logging
 from .timer import Timer, TimingData
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["Timer", "TimingData", "setup_logging", "format_time"]
